cd9e76fb1cf7b985d7a7f71dcbdf7a9659a48110
totikuma <43670695+totikuma@users.noreply.github.com>
2020-07-28 15:19:00 +0900

    v03

---
+++ dist/assets/scripts/tab.js
@@ -0,0 +1,14 @@
+$('.tab-content>div').hide();
+$('.tab-content>div').first().slideDown();
+  $('.tab-buttons span').click(function(){
+    var thisclass=$(this).attr('class');
+    $('#lamp').removeClass().addClass('#lamp').addClass(thisclass);
+    $('.tab-content>div').each(function(){
+      if($(this).hasClass(thisclass)){
+        $(this).fadeIn(800);
+      }
+      else{
+        $(this).hide();
+      }
+    });
+  });
